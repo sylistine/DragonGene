bd60749c0daba92f3145f02b237579124de816d3
hull-aaron <hull_aaron@cygames.co.jp>
2015-10-29 12:27:26 +0900

    Improvements and Bugfixes
    Fixed a bug resulting from an undefined props object during inheritance
    Added a syntax for static properties

--- dragon-gene.js
+++ dragon-gene.js
@@ -1,34 +1,45 @@
-/*
- * DragonGene gives you a simple starting place for class-like inheritance seen in other languages like C++ and Python
- * To create a class inheritance, simply call `.inherit()` on any DragonGene class w/ an (optional) object in JSON or JSLiteral Notation
+/**
+ * DragonGene gives you a simple starting place for class-like inheritance seen
+ * in other languages like C++ and Python
+ * To create a class inheritance, simply call `.inherit()` on any DragonGene
+ * class w/ an (optional) object in JSON or JSLiteral Notation
  * Instantiate your new class by calling `new <ClassName>()`
  */
 var DragonGene = function() {};
 
-/*
+/**
  * Used for creating new Classes
  *
- * @param extensionProps {object} [optional] All of your new class properties, methods, etc.
+ * @param props {object} [optional] new class properties, methods, etc.
  */
-DragonGene.inherit = function(extensionProps) {
+DragonGene.inherit = function(props) {
   var self = this,
     Class = function(){};
 
-  // Static
-  Class.inherit = self.inherit;
-
-  // Instance
+  // Must prepare prototype so that we can inherit and override instance vars
+  // (Not to mention instantiate the class.)
   Class.prototype = {};
 
   var prop;
-  // Inherit props.
-  for(prop in self.prototype) {
-    Class.prototype[prop] = self.prototype[prop];
-  }
 
-  // Override props.
-  for(prop in extensionProps) {
-    Class.prototype[prop] = extensionProps[prop];
+  // Inherit static props.
+  for(prop in self)
+    Class[prop] = self[prop];
+
+  // Inherit instance props.
+  for(prop in self.prototype)
+    Class.prototype[prop] = self.prototype[prop];
+
+  if(props !== void 0) {
+    // Override static props.
+    if(props.hasOwnProperty("static"))
+      for(prop in props.static)
+        Class[prop] = props.static[prop];
+
+    // Override instance props.
+    for(prop in props)
+      if(prop !== 'static')
+        Class.prototype[prop] = props[prop];
   }
 
   return Class;
